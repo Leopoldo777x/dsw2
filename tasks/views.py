d7a2e139453e9d242bc705e0996e269af348e76f
Leonardo Rodriguez <onardojose444@gmail.com>
2025-10-13 15:48:15 +0100

    detallando

--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,10 @@ def task_list(request):
     tasks = Task.objects.all()
 
     return render(request, 'tasks/task/list.html', {'tasks': tasks})
+
+
+def task_detail(request, task_slug: str):
+    try:
+        task = Task.objects.get(slug=task_slug)
+    except Task.DoesNotExist:
+        print('bazuco')
